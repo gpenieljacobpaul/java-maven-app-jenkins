e9b2d0e8dd07bca961447c1c445e66fba51e9035
G Peniel Jacob Paul <32905390+gpenieljacobpaul@users.noreply.github.com>
2025-10-24 21:05:43 +0530

    Update Docker image version from 5.0 to 1.0

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t gpenieljacobpaul/docker-java-maven-app:5.0 .'
+        sh 'docker build -t gpenieljacobpaul/docker-java-maven-app:1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push gpenieljacobpaul/docker-java-maven-app:5.0'
+        sh 'docker push gpenieljacobpaul/docker-java-maven-app:1.0'
         }
     }
 
